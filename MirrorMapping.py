25e4e8c1e13b9d163a404dab3a9965062814a94b
Shubham Mahalle <shubhammahallee@gmail.com>
2026-07-01 09:53:29 +0530

    Update MirrorMapping.py

--- MirrorMapping.py
+++ MirrorMapping.py
@@ -1,1 +1,1 @@
-
+'''Given a string and a number N, we need to mirror the characters from the N-th position up to the length of the string in alphabetical order. In mirror operation, we change 'a' to 'z', 'b' to 'y' and so on.'''
