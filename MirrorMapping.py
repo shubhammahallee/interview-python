c18f557f362f53f043ef358ba782d2c0ec620533
Shubham Mahalle <shubhammahallee@gmail.com>
2026-07-01 10:04:39 +0530

    Update MirrorMapping.py

--- MirrorMapping.py
+++ MirrorMapping.py
@@ -1,31 +1,20 @@
-'''Given a string and a number N, we need to mirror the characters from the N-th position up to the length of the string in alphabetical order. In mirror operation, we change 'a' to 'z', 'b' to 'y' and so on.'''
+input_string = input("Enter String: ")
+n = int(input("Enter N: "))
 
-input_string = input("EnterString: ")
-n = int(input("Enter n: "))
-
-alpha = 'abcdefghijklmnopqrstuvwxyz'
+alpha = "abcdefghijklmnopqrstuvwxyz"
 reverse = alpha[::-1]
-dict1 = dict(zip(alpha,reverse))
 
-prefix = input_string[0:n-1]
+# Create mirror mapping
+mirror_map = dict(zip(alpha, reverse))
+
+# Split string
+prefix = input_string[:n-1]
 suffix = input_string[n-1:]
 
+# Mirror remaining characters
 mirror = ""
-for i in range(0,len(suffix)):
-  mirror = mirror + dict1[suffix[i]]
+for ch in suffix:
+    mirror += mirror_map[ch]
 
-res = prefix + mirror
-print(res)
-
-
-
-
-
-
-
-
-
-
-
-
-
+# Final result
+print(prefix + mirror)
