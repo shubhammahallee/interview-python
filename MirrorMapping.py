0255f94c851047169772055576225ba7f08c0722
Shubham Mahalle <shubhammahallee@gmail.com>
2026-07-01 09:59:02 +0530

    Update MirrorMapping.py

--- MirrorMapping.py
+++ MirrorMapping.py
@@ -1,1 +1,19 @@
 '''Given a string and a number N, we need to mirror the characters from the N-th position up to the length of the string in alphabetical order. In mirror operation, we change 'a' to 'z', 'b' to 'y' and so on.'''
+
+input_string = input("EnterString: ")
+n = int(input("Enter n: "))
+
+alpha = 'abcdefghijklmnopqrstuvwxyz'
+reverse = alpha[::-1]
+dict1 = dict(zip(alpha,reverse))
+
+
+
+
+
+
+
+
+
+
+
