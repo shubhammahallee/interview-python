0de3bd294241d1266ef65a8960d8e7ce43f63cec
Shubham Mahalle <shubhammahallee@gmail.com>
2026-07-28 21:55:42 +0530

    Update SpyNumber.py

--- SpyNumber.py
+++ SpyNumber.py
@@ -1,15 +1,1 @@
-num = 123
-temp = num
-sum = 0
-pro = 1
 
-while temp > 0:
-    digit = temp % 10
-    sum += digit
-    pro *= digit
-    temp //= 10
-
-if sum == pro:
-    print("Spy Number")
-else:
-    print("Not Spy Number")
