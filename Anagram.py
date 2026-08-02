b878b88100e6097ebb21fa4a6756d3de8b7d5fa6
Shubham Mahalle <shubhammahallee@gmail.com>
2026-08-02 17:00:44 +0530

    Update Anagram.py

--- Anagram.py
+++ Anagram.py
@@ -6,3 +6,28 @@ def anagram(str1, str2):
     return sorted(str1) == sorted(str2)
  
 print(anagram(str1,str2))
+
+------------------------------------------------------------------------
+
+
+
+num = 153
+temp = num
+count = 0
+
+while temp > 0:
+    count += 1
+    temp //= 10
+
+temp = num
+arm_sum = 0
+
+while temp > 0:
+    digit = temp % 10
+    arm_sum += digit ** count
+    temp //= 10
+
+if arm_sum == num:
+    print("Armstrong Number")
+else:
+    print("Not Armstrong Number")
